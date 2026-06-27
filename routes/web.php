1a2b11b0e9576e2d4f8c7b2320e1e304103c3585
禹平/發芽的人/臭執著仔/發芽的白學家/天羽波堤 <wangchinming02@gmail.com>
2026-06-27 13:14:01 +0800

    Update web.php for API-only mode

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-// 直接回傳檔案內容
+// 移除掉那些 file_get_contents 的路徑
+// 改為簡單的回應，確認後端還活著就好
 Route::get('/', function () {
-    return file_get_contents(public_path('yuume-frontend/index.html'));
-});
-
-Route::get('/admin', function () {
-    return file_get_contents(public_path('yuume-frontend/admin.html'));
-});
-
-Route::get('/admin_photo', function () {
-    return file_get_contents(public_path('yuume-frontend/admin_photo.html'));
+    return response()->json(['message' => 'Yuume Backend API is running!']);
 });
